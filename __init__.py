2ad8a270d15476c57e8ce96c2df07e87c2552d89
xssoft <116801300+xssoft2022@users.noreply.github.com>
2022-11-20 18:12:27 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
 import turtle as t
 import time
 
-class xs海龟():
-    def 画正方形(a):
-        for i in range(4) :
-            t.forward(a)
-            t.left(90)
-        t.done()
+class xsturtle():
+    def 画正方形(a,b):
+        if b == None :
+            for i in range(4) :
+                t.forward(a)
+                t.left(90)
+            else:
+                for i in range(2) :
+                    t.forward(a)
+                    t.left(90)
+                    t.forward(b)
+                    t.left(90)
+            t.done()
 
     def 画圈(self, extent = None, steps = None):
         t.circle(self , extent , steps)
@@ -28,37 +35,37 @@ class xs海龟():
             t.forward(b)
             t.left(90)
         t.done()
-    def 画三角形(a,d):
+    def triangle(a,d):
         t.forward(a)
         t.left(d)
         t.forward(a)
         t.goto(0,0)
         t.done()
-    def 海龟前进(distance):
-        t.forward(distance)
-    def 海龟右转(degree):
-        t.right(degree)
-    def 海龟左转(degree):
-        t.left(degree)
-    def 海龟前往(x,y):
-        t.goto(x,y)
+#     def 海龟前进(distance):
+#         t.forward(distance)
+#     def 海龟右转(degree):
+#         t.right(degree)
+#     def 海龟左转(degree):
+#         t.left(degree)
+#     def 海龟前往(x,y):
+#         t.goto(x,y)
 
-'''def 转文本(byte):
-    return 'str(byte)'
+# '''def 转文本(byte):
+#     return 'str(byte)'
 
-def 跳出循环():
-    return 'break'
+# def 跳出循环():
+#     return 'break'
 
-def 循环执行(frequency):
-    return 'for i in range (frequency):+    '
+# def 循环执行(frequency):
+#     return 'for i in range (frequency):+    '
 
-无限循环 = 'while True:'''
-def 打印(content):
-    print(content)
+# 无限循环 = 'while True:'''
+# def 打印(content):
+#     print(content)
 
-def 导库(name) :
-    return 'import {name}'
+# def 导库(name) :
+#     return 'import {name}'
 
-def 等待(s) :
-    time.sleep(s)
+# def 等待(s) :
+#     time.sleep(s)
 
